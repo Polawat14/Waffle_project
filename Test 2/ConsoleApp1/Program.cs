8745ab2b89e0f8c0152d85cd1eaba2709c6bee54
D4bbyDoo <118412858+Polawat14@users.noreply.github.com>
2023-04-11 19:00:47 +0700

    11/4/23'

--- Test 2/ConsoleApp1/Program.cs
+++ Test 2/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 String name = "Polawat Gopatta";
 Console.WriteLine("My Nsame is " + name);
+Console.WriteLine("I hate you");
 
 Console.ReadLine();
 
